2b62dc4d8ffc53334db7e31998d51006395bbc79
flrd <m4rkus.doering@gmail.com>
2022-02-07 23:25:51 +0100

    add data about covid vaccinations in US

---
+++ data-raw/covid_vac.R
@@ -0,0 +1,44 @@
+# https://covid.cdc.gov/covid-data-tracker/#rates-by-vaccine-status
+
+url <- "https://data.cdc.gov/api/views/3rge-nu2a/rows.csv?accessType=DOWNLOAD"
+cdc_raw <- read.csv(url)
+
+
+# subset data -------------------------------------------------------------
+# don't distinguish between age groups and vaccine
+cdc <- subset(cdc_raw,
+              subset = Age.group == "all_ages_adj" & Vaccine.product == "all_types",
+              select = c("outcome", "month", "MMWR.week","Age.adjusted.vax.IR", "Age.adjusted.unvax.IR")
+              )
+
+# clean col names ---------------------------------------------------------
+vac_idx <- which(names(cdc) %in% c("Age.adjusted.vax.IR", "Age.adjusted.unvax.IR"))
+names(cdc)[vac_idx] <- paste("value", c("fully.vaccinated", "unvaccinated"), sep = "_")
+
+
+# reshape to long ---------------------------------------------------------
+covid_vac <- reshape(cdc[cols], direction = "long",
+                     idvar = c("outcome", "month", "MMWR.week"),
+                     varying = c("value_fully.vaccinated", "value_unvaccinated"),
+                     sep = "_")
+row.names(covid_vac) <- NULL
+
+
+# rename col names --------------------------------------------------------
+names(covid_vac) <- c("outcome", "month", "year_week", "status", "value")
+
+
+# fully.vaccinated --> fully vaccinated -----------------------------------
+covid_vac$status <- replace(covid_vac$status, covid_vac$status == "fully.vaccinated", "fully vaccinated")
+
+# add a date column -------------------------------------------------------
+covid_vac$date <- as.Date(paste0(covid_vac$year_week, "01"), "%Y%U%w")
+
+
+# save as .csv ------------------------------------------------------------
+write.csv(covid_vac, "data-raw/covid_vac.csv", row.names = FALSE)
+
+
+# save as .rda in /data directory -----------------------------------------
+usethis::use_data(covid_vac, overwrite = TRUE)
+
