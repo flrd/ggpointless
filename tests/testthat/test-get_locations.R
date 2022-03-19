80831fff816fa4e87c22e2e7d72adcfcbec47bca
flrd <m4rkus.doering@gmail.com>
2022-03-19 23:07:37 +0100

    styling

--- tests/testthat/test-get_locations.R
+++ tests/testthat/test-get_locations.R
@@ -2,15 +2,17 @@ test_that("get_locations(..., c('all')) returns 4 rows", {
 
   # generate data
   tmp <- seq(5, -1, length.out = 100) * pi
-  spiral <- data.frame(x = round(sin(tmp) * 1:100, 4),
-                       y = round(cos(tmp) * 1:100, 4))
+  spiral <- data.frame(
+    x = round(sin(tmp) * 1:100, 4),
+    y = round(cos(tmp) * 1:100, 4)
+  )
 
   expect_equal(
     get_locations(spiral, location = c("all")),
     structure(list(x = c(0, -7.9847, 0, 0), y = c(-1, 83.6196, -100,
     -100), location = structure(c(1L, 4L, 3L, 2L), .Label = c("first",
-    "last", "minimum", "maximum"), class = "factor")), class = "data.frame", row.names = c("1",
-    "84", "100", "100.1"))
+    "last", "minimum", "maximum"), class = "factor")), class = "data.frame",
+    row.names = c("1", "84", "100", "100.1"))
   )
 })
 
--- tests/testthat/test-stat-pointless.R
+++ tests/testthat/test-stat-pointless.R
@@ -1,12 +1,17 @@
 test_that("order of ploting is first > last > minimum > maximum", {
-  df1 <- data.frame(x = c(1, 2, 3, 4, 5),
-                    y = c(1, 2, 1, 4, 1))
+  df1 <- data.frame(
+    x = c(1, 2, 3, 4, 5),
+    y = c(1, 2, 1, 4, 1)
+  )
 
-  p <- ggplot(df1, aes(x, y)) + stat_pointless(aes(colour = after_stat(location)), location = "all")
+  p <- ggplot(df1, aes(x, y)) +
+    stat_pointless(aes(colour = after_stat(location)), location = "all")
   x <- layer_data(p)
-  expect_equal(x$location,
-               factor(c("minimum", "first", "minimum", "maximum", "minimum", "last"),
-                      levels = c("first", "last", "minimum", "maximum"))
+  expect_equal(
+    x$location,
+    factor(c("minimum", "first", "minimum", "maximum", "minimum", "last"),
+      levels = c("first", "last", "minimum", "maximum")
+    )
   )
 })
 
@@ -15,14 +20,15 @@ test_that("geom_pointless and stat_pointless produce same layer data", {
   df1 <- data.frame(x = 1:10, y = sample(1:10))
   p1 <- ggplot(df1, aes(x, y)) +
     stat_pointless(aes(color = after_stat(location)),
-                   location = c("first", "last"))
+      location = c("first", "last")
+    )
   p2 <- ggplot(df1, aes(x, y)) +
     geom_pointless(aes(color = after_stat(location)),
-                   location = c("first", "last"))
+      location = c("first", "last")
+    )
 
   expect_equal(
     layer_data(p1),
     layer_data(p2)
   )
-  })
-
+})
