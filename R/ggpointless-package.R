4038127faefae5daae6d400f2aa3a0889260a494
flrd <m4rkus.doering@gmail.com>
2022-03-19 21:54:15 +0100

    list dependencies separately

---
+++ R/ggpointless-package.R
@@ -0,0 +1,12 @@
+#' Additional Geoms, Stats, & Scales for 'ggplot2'
+#'
+#' A package containing additional geoms, coords, stats, scales & fonts
+#' for ggplot2 2.0+
+#'
+#' @name ggpointless
+#' @docType package
+#' @author Markus Döring
+#' @import ggplot2
+#' @importFrom scales trans_new breaks_extended	number_format
+#' @importFrom grid grobTree gList gpar pointsGrob segmentsGrob
+NULL
