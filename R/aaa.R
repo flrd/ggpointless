58445c12a5c623e1b8158adf7e40a1a06c04da30
flrd <m4rkus.doering@gmail.com>
2022-03-19 21:57:12 +0100

    document ggproto objects

---
+++ R/aaa.R
@@ -0,0 +1,12 @@
+#' Base ggproto classes for ggpointless
+#'
+#' If you are creating a new geom, stat, position, or scale in another package,
+#' you'll need to extend from `ggplot2::Geom`, `ggplot2::Stat`,
+#' `ggplot2::Position`, or `ggplot2::Scale`.
+#'
+#' More info at [ggplot2-book.org](https://ggplot2-book.org/)
+#'
+#' @seealso ggproto
+#' @keywords internal
+#' @name ggpointless-ggproto
+NULL
