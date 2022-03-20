bc55e55d8b708ebeb7a55fb578a7402a59c2a798
flrd <m4rkus.doering@gmail.com>
2022-03-20 13:38:31 +0100

    add snapshot tests for key glyph

---
+++ tests/testthat/test-legend-draw.R
@@ -0,0 +1,25 @@
+# Setting of legend key glyphs has to be tested visually
+
+test_that("alternative key glyphs work", {
+  df1 <- data.frame(x = 1:3, xend = 4:6, z = letters[1:3])
+
+  # specify key glyph by name
+  vdiffr::expect_doppelganger("pointrange and lexis key glyphs",
+                      ggplot(df1, aes(x = x, y = xend)) +
+                        geom_line(aes(color = "line"),
+                                  key_glyph = "pointrange") +
+                        geom_point(aes(fill = z),
+                                   pch = 21,
+                                   size = 2,
+                                   key_glyph = "lexis")
+  )
+
+  # specify key glyph by function
+  vdiffr::expect_doppelganger("lexis key glyphs",
+                      ggplot(df1, aes(x = x, xend = xend)) +
+                        geom_lexis(aes(fill = z),
+                                   pch = 21,
+                                   point.size = 3,
+                                   stroke = 1)
+  )
+})
