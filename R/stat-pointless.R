1d6d4ea583f8ed2a70e48498a75ba3847078919c
flrd <m4rkus.doering@gmail.com>
2022-03-31 22:27:31 +0200

    moves helpers to stats

--- R/stat-pointless.R
+++ R/stat-pointless.R
@@ -57,3 +57,65 @@ StatPointless <- ggproto("StatPointless", Stat,
   },
   required_aes = c("x", "y")
 )
+
+#' Subset input data based on locations
+#'
+#' @description
+#' Given a data frame, this functions returns a subset. Returns a
+#' data frame with either "first" row, "last" row and/or the row(s)
+#' that contain minima or maxima
+#'
+#' @param data A `data.frame`
+#' @param location A character string specifying which rows to return:
+#'  "first", "last" (default), "minimum", "maximum" or "all"
+#' @return A data.frame
+#'
+#' @keywords internal
+get_locations <- function(data = NULL,
+                          location = c(
+                            "first",
+                            "last",
+                            "minimum",
+                            "maximum",
+                            "all"
+                          )) {
+  if (is.null(data) || !is.data.frame(data) || nrow(data) == 0) {
+    stop("Please provide a valid data frame.")
+  }
+
+  locations <- match.arg(location, several.ok = TRUE)
+
+  if ("all" %in% location) {
+    locations <- c("first", "last", "minimum", "maximum")
+  }
+
+  # get row indices
+  first_row <- 1
+  n_rows <- nrow(data)
+
+  lst <- list(
+    first = first_row,
+    last = n_rows,
+    minimum = which(data$y == min(data$y, na.rm = TRUE)),
+    maximum = which(data$y == max(data$y, na.rm = TRUE))
+  )
+
+  # extract only desired locations
+  lst <- lst[locations]
+
+  # creates two column data frame of row indices and locations
+  tmp <- utils::stack(lst)
+
+  # make sure that first is plotted on top of last, minimum, maximum,
+  # in that order hence, order by row index, i.e. "values",
+  # then by reversed location level
+  tmp <- tmp[order(tmp[["values"]], -as.integer(tmp[["ind"]])), , drop = FALSE]
+
+  # return subset of input data
+  return(
+    cbind(
+      data[tmp[["values"]], , drop = FALSE],
+      location = tmp[["ind"]]
+    )
+  )
+}
--- R/utils.R
+++ R/utils.R
@@ -2,68 +2,6 @@
   if (!is.null(a)) a else b
 }
 
-#' Subset input data based on locations
-#'
-#' @description
-#' Given a data frame, this functions returns a subset. Returns a
-#' data frame with either "first" row, "last" row and/or the row(s)
-#' that contain minima or maxima
-#'
-#' @param data A `data.frame`
-#' @param location A character string specifying which rows to return:
-#'  "first", "last" (default), "minimum", "maximum" or "all"
-#' @return A data.frame
-#'
-#' @keywords internal
-get_locations <- function(data = NULL,
-                          location = c(
-                            "first",
-                            "last",
-                            "minimum",
-                            "maximum",
-                            "all"
-                          )) {
-  if (is.null(data) || !is.data.frame(data) || nrow(data) == 0) {
-    stop("Please provide a valid data frame.")
-  }
-
-  locations <- match.arg(location, several.ok = TRUE)
-
-  if ("all" %in% location) {
-    locations <- c("first", "last", "minimum", "maximum")
-  }
-
-  # get row indices
-  first_row <- 1
-  n_rows <- nrow(data)
-
-  lst <- list(
-    first = first_row,
-    last = n_rows,
-    minimum = which(data$y == min(data$y, na.rm = TRUE)),
-    maximum = which(data$y == max(data$y, na.rm = TRUE))
-  )
-
-  # extract only desired locations
-  lst <- lst[locations]
-
-  # creates two column data frame of row indices and locations
-  tmp <- utils::stack(lst)
-
-  # make sure that first is plotted on top of last, minimum, maximum,
-  # in that order hence, order by row index, i.e. "values",
-  # then by reversed location level
-  tmp <- tmp[order(tmp[["values"]], -as.integer(tmp[["ind"]])), , drop = FALSE]
-
-  # return subset of input data
-  return(
-    cbind(
-      data[tmp[["values"]], , drop = FALSE],
-      location = tmp[["ind"]]
-    )
-  )
-}
-
 
 # create a decade from a number -------------------------------------------
 #' Given a year, get the decade
@@ -98,60 +36,8 @@ get_decades <- function(years, anno_domini = FALSE) {
   out
 }
 
-
-# get 'lifelines' ---------------------------------------------------------
-#' Given a year, get the decade
-#'
-#' @param x A vector of mode numeric
-#' @param xend A vector of mode numeric
-#' @return A data.frame
-#'
-#' @keywords internal
-get_lexis <- function(x, xend) {
-  if (is.character(x) || is.character(xend)) {
-    stop("`x` and `xend` must be continuous.")
-  }
-
-  if (!(mode(c(x, xend)) == "numeric")) {
-    stop("`x` and `xend` must be continuous.")
-  }
-
-  if (anyNA(x)) {
-    stop("`x` must not contain missing values.")
-  }
-
-  if (any(x > xend, na.rm = TRUE)) {
-    stop("For each row in your data, `xend` must be greater than `x`, or NA")
-  }
-
-  # get all x-postions
-  tmp_x <- sort(c(x, xend))
-
-  # get the y-positions
-  # unclass because cumsum won't work if we work with difftime objects
-  tmp_y <- cumsum(unclass(xend - x))
-  tmp_y <- sort(c(0, tmp_y[-length(tmp_y)], tmp_y))
-
-  # collect xy-coordinates
-  out <- data.frame(
-    x = tmp_x[-length(tmp_x)],
-    xend = tmp_x[-1],
-    y = tmp_y[-length(tmp_y)],
-    yend = tmp_y[-1]
-  )
-
-  # check y and yend positions are the same, if so, assign
-  # dotted linetype to this segment, else solid
-  # Note: we need to assign 'real' linetypes here otherwise we'd
-  # run into an error if we want to use the "type" column from the
-  # layer data and map it to an aesthetic
-  out[["type"]] <- c("solid", "dotted")[(out[["yend"]] - out[["y"]] == 0) + 1]
-  return(out)
-}
-
-
 # translate shape strings - 'borrowed' from ggplot2 -----------------------
-#' Given a year, get the decade
+#' Given a number, get the corresponding point shape
 #'
 #' @param shape_string character
 #' @return A data.frame
