7af53009cb95b07eec45c858a9bd57dc736ca5ba
flrd <m4rkus.doering@gmail.com>
2022-04-04 13:37:13 +0200

    fix pkgdown issue

--- R/ggpointless-package.R
+++ R/ggpointless-package.R
@@ -3,9 +3,6 @@
 
 #' Additional Geoms, Stats for 'ggplot2' 3.3+
 #'
-#' @name ggpointless
-#' @docType package
-#' @author Markus Döring
 #' @import ggplot2
 #' @importFrom scales trans_new breaks_extended	number_format
 #' @importFrom grid grobTree gList gpar pointsGrob segmentsGrob
