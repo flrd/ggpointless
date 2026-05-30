c16cdad99545d02823fe402cc509886e3aed2a9b
flrd <m4rkus.doering@gmail.com>
2026-05-30 17:06:48 +0200

    remove stale files

--- ggpointless.Rcheck/00_pkg_src/ggpointless/tests/testthat/test-lerp.R
+++
@@ -1,5 +0,0 @@
-test_that("linear interpolation works", {
-  expect_equal(2, lerp(a = 2, b = 1, ratio = 0))
-  expect_equal(1, lerp(a = 2, b = 1, ratio = 1))
-  expect_equal(1.75, lerp(a = 2, b = 1, ratio = 0.25))
-})
